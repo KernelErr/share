8ee4664cedf0077332ce9c614e5cca151fdf70c7
hyiker <hyiker@bupt.edu.cn>
2021-05-17 17:00:19 +0800

    frontend initial commit

---
+++ app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <Container :hasLogin="true" :isAdmin="true"></Container>
+</template>
+
+<script>
+import Container from "./components/Container.vue";
+
+export default {
+    name: "App",
+    components: {
+        Container,
+    },
+};
+</script>
+
+<style>
+</style>
